1c2bff67262575fd605163c79c21297c0e88ddf5
Caio Hatai <caio.haruo@hotmail.com>
2023-09-11 10:11:52 -0300

    Style status bar

--- App.tsx
+++ App.tsx
@@ -8,6 +8,7 @@ import { ThemeProvider } from 'styled-components';
 import { Loading } from '@components/Loading';
 import Groups from '@screens/Groups';
 import theme from './src/theme';
+import { StatusBar } from 'react-native';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -17,6 +18,11 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
       {fontsLoaded ? <Groups /> : <Loading />}
     </ThemeProvider>
   );
