51e95d791cf4f8746b7eedfaef0ff73d8e9fffce
Caio Hatai <caio.haruo@hotmail.com>
2023-09-11 15:38:24 -0300

    Create button

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -6,6 +6,7 @@ import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
 import { FlatList } from 'react-native';
 import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 export default function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -25,6 +26,8 @@ export default function Groups() {
           <ListEmpty message="Que tal cadastrar a primeira turma?" />
         )}
       />
+
+      <Button title="Criar nova turma" />
     </Container>
   );
 }
