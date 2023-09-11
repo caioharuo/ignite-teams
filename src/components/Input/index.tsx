9508d937982629160e57577a6f6008f134d870ca
Caio Hatai <caio.haruo@hotmail.com>
2023-09-11 17:14:57 -0300

    Style new group input placeholder

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,6 +1,10 @@
 import { TextInputProps } from 'react-native';
+import { useTheme } from 'styled-components/native';
+
 import { Container } from './styles';
 
 export function Input({ ...rest }: TextInputProps) {
-  return <Container {...rest} />;
+  const theme = useTheme();
+
+  return <Container placeholderTextColor={theme.COLORS.GRAY_300} {...rest} />;
 }
