c0c82d86a0352da821a2d84406998e06172a77db
Caio Hatai <caio.haruo@hotmail.com>
2023-09-11 14:36:04 -0300

    Add groups state

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,25 @@
-import { Header } from '@components/Header';
+import { useState } from 'react';
+
 import { Container } from './styles';
+import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { FlatList } from 'react-native';
 
 export default function Groups() {
+  const [groups, setGroups] = useState<string[]>(['Grupo 1', 'Grupo 2']);
+
   return (
     <Container>
       <Header />
 
       <Highlight title="Turmas" subtitle="jogue com a sua turma" />
 
-      <GroupCard title="Oi" onPress={() => console.log('Hello')} />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+      />
     </Container>
   );
 }
